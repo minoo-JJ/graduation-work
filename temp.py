b108766cc35a0024d67b36192d070dd80d65e7ae
minoo-JJ <80943639+minoo-JJ@users.noreply.github.com>
2021-09-12 05:02:29 +0900

    코드완성

--- temp.py
+++ temp.py
@@ -18,7 +18,7 @@ def normalize(_list):
         normalized.append(normalized_x)
     return np.array(normalized)
 
-#하이퍼파라미터 설정
+#하이퍼파라미터 설정하기
 
 
 np.random.seed(100)
@@ -52,49 +52,16 @@ x_test = test[:,:-1]
 x_test = x_test[:, :, np.newaxis]
 y_test = test[:,-1]
 
-'''
-x_trained = []
-x_test = []
- 
-y_trained = data['Adj Close'].values[60:1414+60]
-#y_trained = pd.DataFrame(data=y_trained)
-
-y_test = data['Adj Close'].values[1414+60:1599]
-#y_test = pd.DataFrame(data=y_test)
-
-for i in range(len(y_trained)):
-    x_trained.append(data['Adj Close'].values[i:i+60])    
-#df_trained = pd.DataFrame(data = np.c_[x_trained, y_trained], columns = np.linspace(-60, 0, 61))
-
-x_trained = normalize(x_trained)
-x_trained = x_trained[:, :, np.newaxis]
-#print(x_trained.shape, y_trained.shape)
-
-for i in range(len(y_test)):
-    x_test.append(data['Adj Close'].values[1474-60+i:1474+i])
-#df_test = pd.DataFrame(data = np.c_[x_test, y_test], columns = np.linspace(-60, -1, 61))
-
-x_test = normalize(x_test)
-x_test = x_test[:, :, np.newaxis]
-#print(x_test.shape, y_test.shape)
-
-plt.subplot(2,1,1)
-plt.plot(y_trained, linewidth=0.5)
-plt.subplot(2,1,2)
-plt.plot(y_test, linewidth=1)
-#plt.plot(y_test, 'b', linewidth=1)
-'''
-
 model = Sequential()
-model.add(LSTM(60, input_shape=(60,1)))
+model.add(LSTM(100, input_shape=(60,1)))
 #model.add(Dropout(0.5))
-model.add(Dense(80, activation='relu'))
+model.add(Dense(120, activation='relu'))
 model.add(Dense(1, activation='linear'))
 
 model.compile(optimizer='adam', loss='mse', metrics=['mae'])
 
 model.summary()
-hist = model.fit(x_train, y_train, batch_size=20, epochs=100, validation_data=(x_vld, y_vld))
+hist = model.fit(x_train, y_train, batch_size=10, epochs=100, validation_data=(x_vld, y_vld))
 #%%
 fig, ax = plt.subplots(2,1)
 
